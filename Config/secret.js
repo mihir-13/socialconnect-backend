32444d24175b73901df26b86d4ca8434754982e6
Mihir <mihirbrahmbhatt3@gmail.com>
2020-06-24 22:01:31 +0530

    heroku node js deploy

--- Config/secret.js
+++ Config/secret.js
@@ -3,7 +3,6 @@
 //   secret: 'applicationisasecretkey'
 // }
 
-
 module.exports = {
   url: 'mongodb+srv://admin-mihir:rastablasta1@cluster0-78ren.mongodb.net/socialconnect',
   secret: 'applicationisasecretkey'
